966ba5f89eb19bada7cc7d70695a0a00d965afea
Piyush Gurjar <piyush.g@consultadd.com>
2025-01-30 11:28:45 +0530

    deleted extra component

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,16 +1,25 @@
-import TaskItems from "./TaskItems";
 import { Task } from "../src/Todo"; 
 type TaskListProps = {
     taskList: Task[]; 
     deleteTask: (taskIndex: number) => void; 
     completeTask: (taskIndex: number) => void; 
+    index: number; 
+    currTask: Task; 
   };
 
 export default function TaskList({taskList,deleteTask,completeTask}: TaskListProps) {
     return (
         <ul>
             {taskList.map((currTask,index)=>{
-               return <TaskItems key = {index} currTask = {currTask} deleteTask={deleteTask} completeTask={completeTask} index = {index}/>
+               return (
+                <div style={{ display: "flex items-center mb-2" }}>
+                <div className={`mx-4 ${currTask.completed ? "line-through text-gray-500" : "text-white"}`}>
+                  {currTask.task}
+                </div>
+                <button onClick={() => completeTask(index)}>Complete</button>
+                <button onClick={() => deleteTask(index)}>Delete</button>
+              </div>
+               )
             })
             }
         </ul>
